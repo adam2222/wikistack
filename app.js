818392bda715d924b168d5eb1c632e605395c04d
Charity Corcino <cecicorcino@gmail.com>
2016-11-14 11:42:15 -0500

    Sqe

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var morgan = require('morgan')
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var routes = require('./routes/index.js');
+var models = require('./models/index.js');
 
 var env = nunjucks.configure("views", {noCache: true});
 app.set('view engine', 'html');
@@ -11,8 +12,21 @@ app.engine('html', nunjucks.render);
 
 app.use(routes)
 app.use(morgan('dev'));
+
+
 app.use(express.static('public'));
 
-app.listen(3000, function(req, res) {
+app.listen(5432, function(req, res) {
 	console.log("We're Listening...");
 });
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(5432, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
